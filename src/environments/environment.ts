0f462fa5d2b7142f1f068b2412d229b89b2197f2
erikjc29 <erikalejc@gmail.com>
2022-10-16 21:49:37 -0300

    actualizacion

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCtlApd6ioIYgvYb24G6Kq1faxrTICDGxE",
+    authDomain: "ionicfirebase-fut.firebaseapp.com",
+    projectId: "ionicfirebase-fut",
+    storageBucket: "ionicfirebase-fut.appspot.com",
+    messagingSenderId: "1035028355164",
+    appId: "1:1035028355164:web:ab9d8eeee566d7c7c5ae99"
+  }
+};
